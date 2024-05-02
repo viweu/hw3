2ebb176ce6fdbba1ffbebe208b8a48c06197b971
viweu <153850831+viweu@users.noreply.github.com>
2024-05-02 18:03:40 +0300

    Update person.java

--- model/person.java
+++ model/person.java
@@ -4,4 +4,22 @@ import java.util.TreeSet;
 public class Person implements Comparable<Person> {
     private String name;
     private String birthDate;
+
+    public Person(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.name.compareTo(other.name);
+    }
+}
