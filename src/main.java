b1febe72f551417589a664d992f1d67363d638fb
viweu <153850831+viweu@users.noreply.github.com>
2024-05-02 17:44:33 +0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        FamilyTree familyTree = new FamilyTree();
+        familyTree.addPerson(new Person("Daniel", new Date()));
+        familyTree.addPerson(new Person("Viktoria", new Date()));
+        // Добавление других членов...
+
+        familyTree.sortByName();
+        for (Person person : familyTree) {
+            System.out.println(person.getName());
+        }
+
+        familyTree.sortByBirthDate();
+        for (Person person : familyTree) {
+            System.out.println(person.getName());
+        }
+    }
+}
